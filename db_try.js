d8e94a43b051312c7a2ba27a09f25024af5d2e2f
Masnoon Nayem Khan <61927060+TheGhaplaMan@users.noreply.github.com>
2022-02-02 16:53:17 -0800

    same prob lol

--- db_try.js
+++ db_try.js
@@ -2,31 +2,57 @@ const express = require('express')
 const Mongopepol = require('mongodb').MongoClient
 const app = express()
 const port = 3000
-const url = "mongodb+srv://TheGhaplaMan:Lu Yb Tm@cluster0.pnyuc.mongodb.net/dbName?retryWrites=true&w=majority"
+const url = "mongodb+srv://TheGhaplaMan:Lu Yb Tm@cluster0.pnyuc.mongodb.net/dbNami?retryWrites=true&w=majority"
+const client = new Mongopepol(url);
 
-Mongopepol.connect(url, (err, db) => {
-    if (err) throw err;
-    console.log("working");
-    const dbo = db.db('dbName');
-    dbo.createCollection("newCollection", function(err, res) {
-        if (err) throw err;
-        console.log("Collection created!");
-        db.close();
-      });
+//w3school
+// Mongopepol.connect(url, (err, db) => {
+//     if (err) throw err;
+//     console.log("working" , db);
+//     const dbo = db.db('dbNami');
+//     dbo.createCollection("newCollection", function(err, res) {
+//         if (err) throw err;
+//         console.log("Collection created!");
+//         db.close();
+//       });
+//     const dbobj = {
+//         name : "Adit",
+//         "Marital Status" : false
+//     }
+//     dbo.collection("newCollection").insertOne(dbobj, (err, res) => {
+//         if(err) throw err;
+//         console.log(res);
+//     });
+//     db.close();
+// })
+
+//npm official doc
+async function main () {
+    await client.connect();
+    console.log("paiseeeh");
+    const  db = client.db("dbNami");
+    const collection = db.collection("newCollection");
     const dbobj = {
-        name : "Adit",
-        "Marital Status" : false
-    }
-    dbo.collection("newCollection").insertOne(dbobj, (err, res) => {
-        if(err) throw err;
-        console.log(res);
-    });
-    db.close();
-})
+                name : "Adit",
+                "Marital Status" : false
+            }
+    await db.collection("newCollection").insertOne(dbobj, (err,res) => {
+        if(err) console.log(err);
+        console.log(res); 
+    })
+
+    return 'done.';
+}
+
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
+
 
 
 app.get('/', (req, res) => {
-    res.send ("noiceee");
+    res.send ("noiceee"); 
 })
 
 app.listen(port, () => {
