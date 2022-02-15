3d4689edda5361d2f1bc605d4d34d5a888c82942
Masnoon Nayem Khan <61927060+TheGhaplaMan@users.noreply.github.com>
2022-02-14 16:22:17 -0800

    worked on DOM manipulation and api calling (almost)

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,25 @@
-console.log("Ashey kina");
+// const body = document.body
+// const div = document.createElement("div")
+// div.innerText= "lmaaaao"
+// // div.textContent = "Kireh"
+// const sth = document.getElementById("sth");
+// sth.innerText = "Goru Marka";
+// console.log("sth");
+
+// body.append(div)
+
+
+// body.append("lmaaao")
+
+// // const div = document.createElement("div")
+// // document.querySelector('div')
+// // console.log(div.innerText)
+// // console.log(div.textContent)
+
+const newShopForm = document.getElementById("newShop");
+newShopForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(e.target.shopName.value);
+    fetch("http://localhost:3000/api/v1/venue/")
+
+})
