1777d2e76932f7513dda44c4ee5dd65b613c0269
Masnoon Nayem Khan <61927060+TheGhaplaMan@users.noreply.github.com>
2022-03-29 11:00:59 +0600

    config added

--- ECProject/voter.app.js
+++ ECProject/voter.app.js
@@ -7,7 +7,7 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./ECProject/config.env" });
 
 const port = process.env.PORT || 3000;
-const url = process.env.DBURL;
+const url = process.env.EC_DBURL;
 
 app.use(express.static(path.join(__dirname, "public")));
 
