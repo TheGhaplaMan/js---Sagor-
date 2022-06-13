fe9c8c19eda8cdbdba0a676905b93e97b2fbc554
Masnoon Nayem Khan <61927060+TheGhaplaMan@users.noreply.github.com>
2022-06-13 08:54:02 +0600

    bhul api :'3

--- ECProject/client/src/components/AdminLogin/AdminLogin.js
+++ ECProject/client/src/components/AdminLogin/AdminLogin.js
@@ -23,17 +23,20 @@ const AdminLogin = ({ text }) => {
 
     const { email, pass } = info;
     try {
-      // const res = await fetch("http://theghaplaman.herokuapp.com/api/v1/admin/login", {
-      const res = await fetch("http://localhost:4000/api/v1/admin/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email,
-          pass,
-        }),
-      });
+      const res = await fetch(
+        "http://theghaplaman.herokuapp.com/api/v1/admin/login",
+        {
+          // const res = await fetch("http://localhost:4000/api/v1/admin/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email,
+            pass,
+          }),
+        }
+      );
       const data = await res.json();
       // console.log(data);
 
