aa89f60b64cc5b13d902684565368cc74569fccf
Masnoon Nayem Khan <61927060+TheGhaplaMan@users.noreply.github.com>
2022-09-18 23:09:42 +0600

    ...

--- ECProject/client/src/Pages/User/Scan.js
+++ ECProject/client/src/Pages/User/Scan.js
@@ -1,16 +1,27 @@
 import React from "react";
+
 import Navigation from "../../components/Navbar/Navbar";
 import RedButton from "../../components/Button/Redbutton";
 import Scanner from "../../components/Scanner/Scanner";
+import { useNavigate } from "react-router-dom";
 
 const Scan = () => {
-  
+  const navigate = useNavigate();
   return (
     <>
       <Navigation text="Scan" />
       <div className="text-center mb-5 mt-5">
-
-      <RedButton toPage="/" btnName="Home" />
+        <button
+          className="btn_shadow"
+          style={{
+            backgroundColor: "#CF8181",
+            color: "#000",
+          }}
+          onClick={() => navigate(-1)}
+        >
+          Back
+        </button>
+        {/* <RedButton onclick=navigate(-1) btnName="Home" /> */}
       </div>
       <Scanner />
     </>
